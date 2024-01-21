7caa42c458f95d1755c4ce20099c3cee14df2484
Zowde <157274222+Zowde@users.noreply.github.com>
2024-01-21 16:47:54 +0200

    Update Coins.java

--- Coins.java
+++ Coins.java
@@ -4,7 +4,7 @@ public static void main(String[] args)
 {
     int quarter=Integer.parseInt(args[0])/25;
      int cent=Integer.parseInt(args[0])%25;
-   System.out.println("Use "+" "+quarter+" "+"quarters"+" "+cent+" "+"cents");
+   System.out.println("Use"+" "+quarter+" "+"quarters"+" "+cent+" "+"cents");
 }
 
 }
